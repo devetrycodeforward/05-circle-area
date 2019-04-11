062b886ab80645b688951420c2148d658b41d11a
mherna42 <49410553+mherna42@users.noreply.github.com>
2019-04-10 22:08:14 -0600

    Update circle_area.py

--- circle_area.py
+++ circle_area.py
@@ -2,23 +2,13 @@
 # TODO: implement the function to RETURN (NOT PRINT) the area of a circle whose radius is r
 # HINT: You'll need to import math to solve this problem correctly.
 
-# Below are some tests which can give you an indication that your code seems to be correct.
 import math
-from test import testEqual
 
 def area_of_circle(r):
     result = math.pi*(r**2)
     return result
 
-t = area_of_circle(0)
-testEqual(t, 0)
-t = area_of_circle(1)
-testEqual(t,math.pi)
-t = area_of_circle(100)
-testEqual(t, 31415.926535897932)
-t = area_of_circle(-1)
-testEqual(t, math.pi)
-t = area_of_circle(-5)
-testEqual(t, 25 * math.pi)
-t = area_of_circle(2.3)
-testEqual(t, 16.61902513749)
+def main():                      # Define the main function
+    print("circle area of radius 3 is ", area_of_circle(3))
+
+main()   
