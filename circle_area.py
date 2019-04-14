94a8b970a3c4af9f94bc89d710d03f4842625961
ShaneBonin <49175483+ShaneBonin@users.noreply.github.com>
2019-04-13 19:37:11 -0600

    4/13/7:37pm

--- circle_area.py
+++ circle_area.py
@@ -1,6 +1,9 @@
 # TODO: DEFINE a function using the def keyword called `area_of_circle`
 # TODO: implement the function to RETURN (NOT PRINT) the area of a circle whose radius is r
 # HINT: You'll need to import math to solve this problem correctly.
+from test import testEqual
+
+import math
 def area_of_circle(r):
     y = (math.pi * r ** 2)
     return y
