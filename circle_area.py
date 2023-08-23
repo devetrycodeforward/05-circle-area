66615f289a0f8109a0ea8c21cb4988060d40e4c3
Fiona Gorrie <47618889+Fiona-Gorrie@users.noreply.github.com>
2023-08-23 21:42:19 +0000

    Update circle_area.py
    Correcting variable name and equation:
    
    previous version was using the function name as a variable within function, this version replaces it with variable "area"
    
    replacing wrong equation "r * r * 3.14" with (correct) "(r ** 2) * math.pi"

--- circle_area.py
+++ circle_area.py
@@ -1,4 +1,8 @@
 import math
-def area_of_circle(r):
-    area_of_circle = r * r * math.pi
-    return area_of_circle
+
+def area_of_circle(r):       # define a function called area_of_circle which takes an argument called r
+    area = (r**2) * math.pi  # area of any circle is equal to the radius squared, multiplied by pi (where pi is 3.14159....
+    #print(area)             # self test pt 1
+    return area              # return the area of a circle whose radius is r
+
+#area_of_circle(2)           # self test pt 2
